3609b3a0275345d497e29978605835166f52c02e
mpacdia <146739066+mpacdia@users.noreply.github.com>
2024-05-05 16:25:15 +0100

    ergwer

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+
 
 public class cameraController : MonoBehaviour
 {
-    public static moving Instance { get; private set;}
+    
+
+    public CinemachineVirtualCamera cameraInitial;
+    public CinemachineVirtualCamera cameraZone1;
+    public CinemachineVirtualCamera cameraZone2;
+    public CinemachineVirtualCamera cameraZone3;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +23,40 @@ public class cameraController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "initialZone")
+        {
+            cameraInitial.Priority = 1;
+            cameraZone1.Priority = 0;
+            cameraZone2.Priority = 0;
+            cameraZone3.Priority = 0;
+        }
+
+        if (other.gameObject.tag == "zone1")
+        {
+            cameraInitial.Priority = 0;
+            cameraZone1.Priority = 1;
+            cameraZone2.Priority = 0;
+            cameraZone3.Priority = 0;
+        }
+        
+        if (other.gameObject.tag == "zone2")
+        {
+            cameraInitial.Priority = 0;
+            cameraZone1.Priority = 0;
+            cameraZone2.Priority = 1;
+            cameraZone3.Priority = 0;
+        }
+
+        if (other.gameObject.tag == "zone3")
+        {
+            cameraInitial.Priority = 0;
+            cameraZone1.Priority = 0;
+            cameraZone2.Priority = 0;
+            cameraZone3.Priority = 1;
+        }
     }
 }
